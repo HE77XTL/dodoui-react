bf71f86ec00bd35edf5ab2da94a50918fb9aec06
HE77XTL <he77505357@163.com>
2020-05-11 00:08:39 +0800

    添加测试，jest

---
+++ lib/icon/__tests__/icon.unit.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Icon from '../icon'
+import * as renderer from 'react-test-renderer'
+import {describe, expect, it} from '@jest/globals'
+
+function add(a: number, b: number) {
+    return a + b
+}
+
+describe('我的第一个测试用例', () => {
+    // @ts-ignore
+    it('add(1,2) 等于3', () => {
+        expect(add(1,2)).toEqual(3);
+    })
+});
+
+describe('icon', () => {
+    it('是个 div', () => {
+        const json = renderer.create(<Icon/>).toJSON();
+        expect(json).toMatchSnapshot()
+    })
+})
