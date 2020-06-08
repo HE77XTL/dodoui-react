d54602f2f4c2bd6835ac60c1b76b675e7736b31d
HE77XTL <he77505357@163.com>
2020-06-09 00:23:45 +0800

    引入 prism-react-renderer 对示例代码进行高亮显示

--- example/demo.tsx
+++ example/demo.tsx
@@ -1,17 +1,29 @@
 import * as React from "react";
+import Highlight, {defaultProps} from "prism-react-renderer";
 
 interface Props {
     code: string
 }
 
 const Demo: React.FunctionComponent<Props> = (props) => {
-    console.log(props.code)
     return (
         <div>
             {props.children}
-            <pre className='demoCode'>
-                {props.code}
-            </pre>
+            <div className='demoCode'>
+                <Highlight {...defaultProps} code={props.code} language="jsx">
+                    {({className, style, tokens, getLineProps, getTokenProps}) => (
+                        <pre className={className} style={style}>
+                        {tokens.map((line, i) => (
+                            <div {...getLineProps({line, key: i})}>
+                                {line.map((token, key) => (
+                                    <span {...getTokenProps({token, key})} />
+                                ))}
+                            </div>
+                        ))}
+                    </pre>
+                    )}
+                </Highlight>
+            </div>
         </div>
     );
 };
