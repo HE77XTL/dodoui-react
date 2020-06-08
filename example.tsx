596d5e9bd7b48c79024c6e129e210809be4d8a84
HE77XTL <he77505357@163.com>
2020-06-08 23:45:01 +0800

    修改官网样式，引入raw-loader 展示源码

--- example.tsx
+++ example.tsx
@@ -2,8 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {
     HashRouter as Router,
-    Route,
-    Link
+    Route, NavLink
 } from "react-router-dom";
 import IconExample from "./example/IconExample";
 import "./example/home.less";
@@ -12,37 +11,41 @@ import LayoutExample from "./example/layoutExample";
 
 ReactDOM.render(
     <Router>
-        <div className='homeDocument'>
-            <div>
-                <header className='homeHeader'>
-                    <div className='title'>DoDo UI</div>
-                    <div className='tips'>指南</div>
-                    <div className='components'>组件</div>
-                </header>
-            </div>
-            <div className='homeContent'>
-                <aside>
-                    <dl className='navList'>
-                        <dt className='nav'>
-                            Basic
-                        </dt>
-                        <dd>
-                            <Link to="/icon">icon</Link>
-                        </dd>
-                        <dd>
-                            <Link to="/dialog">dialog</Link>
-                        </dd>
-                        <dd>
-                            <Link to="/layout">layout</Link>
-                        </dd>
-                    </dl>
-                </aside>
-                <main className='homeMain'>
-                    <Route path="/icon" component={IconExample}/>
-                    <Route path="/dialog" component={DialogExample}/>
-                    <Route path="/layout" component={LayoutExample}/>
-                </main>
-            </div>
+        <div className='dodo-site'>
+            <header className='dodo-header'>
+                <div className='title'>DoDo UI</div>
+                <div className='source'>
+                    <a href="https://github.com/HE77XTL/dodoui-react"
+                       target='_blank'>github</a>
+                </div>
+                <div className='components'>
+                    <img src="./example/avatar.jpg" alt="avatar" className='avatar'/>
+                </div>
+            </header>
+            <aside className='dodo-aside'>
+                <dl className='navList'>
+                    <dt className='menuItem'>
+                        Basic
+                    </dt>
+                    <dd>
+                        <NavLink to="/icon" className='menuItem' activeClassName='active'>icon</NavLink>
+                    </dd>
+                    <dd>
+                        <NavLink to="/dialog" className='menuItem' activeClassName='active'>dialog</NavLink>
+                    </dd>
+                    <dd>
+                        <NavLink to="/layout" className='menuItem' activeClassName='active'>layout</NavLink>
+                    </dd>
+                </dl>
+            </aside>
+            <main className='dodo-main'>
+                <Route path="/icon" component={IconExample}/>
+                <Route path="/dialog" component={DialogExample}/>
+                <Route path="/layout" component={LayoutExample}/>
+            </main>
+            <footer className='dodo-footer'>
+                dodo
+            </footer>
         </div>
     </Router>,
     document.getElementById("root")
--- example/IconExample.tsx
+++ example/IconExample.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Icon from "../lib/icon/icon";
 
+const code = require('!!raw-loader!./IconExample.tsx');
 
 function IconExample() {
     return (
@@ -8,6 +9,9 @@ function IconExample() {
             <Icon name='qq' className='test1' />
             <Icon name='wechat' className='test2 test3'/>
             <Icon name='alipay' className='' onClick={() => {console.log(1)}} />
+            <div>
+                <pre>{code.default}</pre>
+            </div>
         </div>
     );
 }
