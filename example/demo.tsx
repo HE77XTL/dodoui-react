696ba41bb9e90cabe6cd4c650d8d54b38083eab5
HE77XTL <he77505357@163.com>
2020-06-11 00:35:34 +0800

    官网样式优化

--- example/demo.tsx
+++ example/demo.tsx
@@ -1,18 +1,27 @@
 import * as React from "react";
 import Highlight, {defaultProps} from "prism-react-renderer";
+import {useState} from "react";
 
 interface Props {
     code: string
 }
 
 const Demo: React.FunctionComponent<Props> = (props) => {
+    const [codeVisible, setCodeVisible] = useState(true);
+    const codeVisibleToggle = () => {
+        setCodeVisible(!codeVisible);
+    };
+    const codeVisibleClass = codeVisible ? 'expand' : '';
     return (
-        <div>
-            {props.children}
+        <div className='doc-demo'>
+            <div className='component'>
+                {props.children}
+            </div>
             <div className='demoCode'>
-                <Highlight {...defaultProps} code={props.code} language="jsx">
-                    {({className, style, tokens, getLineProps, getTokenProps}) => (
-                        <pre className={className} style={style}>
+                <div className={`codePanel ${codeVisibleClass}`}>
+                    <Highlight {...defaultProps} code={props.code} language="jsx">
+                        {({className, style, tokens, getLineProps, getTokenProps}) => (
+                            <pre className={className} style={style}>
                         {tokens.map((line, i) => (
                             <div {...getLineProps({line, key: i})}>
                                 {line.map((token, key) => (
@@ -21,8 +30,12 @@ const Demo: React.FunctionComponent<Props> = (props) => {
                             </div>
                         ))}
                     </pre>
-                    )}
-                </Highlight>
+                        )}
+                    </Highlight>
+                    <div className='codeVisible' onClick={codeVisibleToggle}>
+                        {codeVisible ? "隐藏代码" : "显示代码"}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- example/iconDemo.tsx
+++ example/iconDemo.tsx
@@ -1,14 +1,29 @@
 import * as React from "react";
 import Demo from "./demo";
 import IconExample from "./iconExample";
+import {scopedClassMaker} from "../lib/helpers/classes";
 
 const code = require("!!raw-loader!./iconExample.tsx");
-
+const sc = scopedClassMaker("doc");
 const IconDemo: React.FunctionComponent = () => {
     return (
-        <Demo code={code.default}>
-            <IconExample/>
-        </Demo>
+        <div className={sc("example")}>
+            <header className={sc("header")}>
+                <h2 className={sc("title")}>icon 图标</h2>
+                <p>提供了一套常用的图标集合。</p>
+            </header>
+            <main>
+                <div className={sc("use")}>
+                    <div className={sc("tip")}>使用方法</div>
+                    <Demo code={code.default}>
+                        <IconExample/>
+                    </Demo>
+                </div>
+                <div className={sc('attributes')}>
+                    Attributes
+                </div>
+            </main>
+        </div>
     );
 };
 
