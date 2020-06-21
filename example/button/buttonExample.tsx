d5a4c7e5620fd6dd1b9e9db1e280dc45d6905f74
hedongsheng <heds@163.com>
2020-06-21 20:46:31 +0800

    button css

--- example/button/buttonExample.tsx
+++ example/button/buttonExample.tsx
@@ -1,14 +1,12 @@
 import DoButton from '../../lib/button/button'
 import * as React from "react";
-import {scopedClassMaker} from "../../lib/helpers/classes";
 import './buttonExample.less'
 
-const sc = scopedClassMaker("doc");
 const ButtonExample = () => {
     return (
-        <div className={sc('buttonWrap')}>
-            <DoButton className={sc('button')}/>
-            <DoButton size='small' btnType='primary' className={sc('button')}/>
+        <div className='buttonWrap'>
+            <DoButton/>
+            <DoButton size='small' btnType='primary'/>
         </div>
     )
 };
