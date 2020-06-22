a0ab361823fb274188357361a320032a84800e9c
HE77XTL <he77505357@163.com>
2020-06-22 23:01:03 +0800

    butoonn icon loading

--- example/button/buttonExample.tsx
+++ example/button/buttonExample.tsx
@@ -6,6 +6,8 @@ const ButtonExample = () => {
     return (
         <div className='buttonWrap'>
             <DoButton>btn</DoButton>
+            <DoButton loading={true}>btn</DoButton>
+            <DoButton icon='wechat'>icon</DoButton>
             <DoButton size='small' btnType='primary'>primary</DoButton>
             <DoButton size='small' btnType='primary' disabled={true}>disabled</DoButton>
         </div>
--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import classes, {scopedClassMaker} from '../helpers/classes'
-import './button.less'
+import React, {Fragment} from "react";
+import classes, {scopedClassMaker} from "../helpers/classes";
+import Icon from "../icon/icon";
+import "./button.less";
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     size?: string,
     btnType?: string,
-    children: string
+    icon?: string,
+    loading?: boolean,
+    iconFill?: string, // iocn 颜色（icon 组件待完善）
 }
 
+// loading 和icon 互斥
+
 const sc = scopedClassMaker("dodo-button");
+const iconSc = scopedClassMaker("dodo-icon");
 const DoButton: React.FunctionComponent<Props> = (props) => {
     const {className, size, btnType, ...rest} = props;
+    const content = typeof props.children === "string" ?
+            <span>{props.children}</span> : props.children;
+    const iconName = props.loading ? "loading" : props.icon;
+    const icon = iconName && <Icon name={iconName} className={iconSc({loading: !!props.loading})}/>;
+    const btnInner = <Fragment>{icon}{content}</Fragment>;
     const classFmt = classes(sc({
         "": true,
-        disabled: !!props.disabled
+        disabled: !!props.disabled,
     }), className, size, btnType);
-
     return (
-        <button className={classFmt} {...rest} disabled={props.disabled}>{props.children}</button>
-    )
+        <button
+            className={classFmt}
+            disabled={props.disabled}
+            {...rest}>
+            {btnInner}
+        </button>
+    );
 };
 DoButton.defaultProps = {
-    size: 'normal'
+    size: "normal"
 };
-export default DoButton
+export default DoButton;
