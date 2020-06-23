cc3f885364cc5ca3ba627ee7b9da4c9b7e8fae57
HE77XTL <he77505357@163.com>
2020-06-23 21:10:04 +0800

    button size

--- example/button/buttonExample.tsx
+++ example/button/buttonExample.tsx
@@ -1,17 +1,25 @@
-import DoButton from '../../lib/button/button'
+import DoButton from "../../lib/button/button";
 import * as React from "react";
-import './buttonExample.less'
+import "./buttonExample.less";
 
+const loading = true;
 const ButtonExample = () => {
     return (
         <div className='buttonWrap'>
-            <DoButton>btn</DoButton>
-            <DoButton loading={true}>btn</DoButton>
-            <DoButton icon='wechat'>icon</DoButton>
-            <DoButton size='small' btnType='primary'>primary</DoButton>
-            <DoButton size='small' btnType='primary' disabled={true}>disabled</DoButton>
+            <div>
+                <DoButton size='mini' icon='wechat'>mini</DoButton>
+                <DoButton size='small' icon='wechat'>small</DoButton>
+                <DoButton icon='wechat'>normal</DoButton>
+                <DoButton size='large' icon='wechat'>large</DoButton>
+            </div>
+            <div>
+                <DoButton btnType='primary'>primary</DoButton>
+                <DoButton icon='wechat'>icon</DoButton>
+                <DoButton loading={loading} disabled={loading}>loading</DoButton>
+                <DoButton btnType='primary' disabled={true}>disabled</DoButton>
+            </div>
         </div>
-    )
+    );
 };
 
-export default ButtonExample
+export default ButtonExample;
