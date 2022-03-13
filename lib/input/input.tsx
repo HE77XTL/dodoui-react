4cc973c57f80ee4099e6e2cb441c420516ca6907
何东升 <he77505357@163.com>
2022-03-13 23:47:01 +0800

    feat(input): 增加password 输入框（待完善）

--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -1,15 +1,17 @@
 import React, {InputHTMLAttributes, useRef, useState} from "react";
-import Icon from '../../lib/icon/icon'
+import Icon from '../../lib/icon/icon';
 import classes, {scopedClassMaker} from "../helpers/classes";
-import './input.less'
+import './input.less';
 
 import {isEmpty} from "../helpers/utils";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-    clearable?: boolean
+    clearable?: boolean,
+    businessType?: 'password'
 }
 
 const DoInput: React.FunctionComponent<Props> = (props) => {
+    const [passwordType, setPasswordType] = useState('show');
     const [clearIconVisible, setClearIconVisible] = useState(getInitIconVisible());
     const refInput = useRef(null);
     const sc = scopedClassMaker("dodo");
@@ -18,20 +20,28 @@ const DoInput: React.FunctionComponent<Props> = (props) => {
 
 
     function onClear(e: React.MouseEvent<SVGGElement>) {
+        console.log(8888);
         if (refInput && refInput.current) {
             // @ts-ignore
-            refInput.current.value = ''
+            refInput.current.value = '';
+            console.log(8989);
+            setClearIconVisible(false);
         }
     }
 
     function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
         onChange && onChange(e);
-        setClearIconVisible(props.clearable && !isEmpty(e.target.value))
+        setClearIconVisible(props.clearable && !isEmpty(e.target.value));
     }
 
     function getInitIconVisible() {
         const value = props.defaultValue || props.value;
-        return props.clearable && !isEmpty(value)
+        return props.clearable && !isEmpty(value);
+    }
+
+    function onPasswordEyeToggle() {
+        const nextType = passwordType === 'show' ? 'hide' : 'show';
+        setPasswordType(nextType);
     }
 
 
@@ -43,8 +53,16 @@ const DoInput: React.FunctionComponent<Props> = (props) => {
                 className={classes(sc("input"), className, clearableClass)}
                 onChange={onInputChange}
             />
-            {clearIconVisible ?
-                <Icon name="clear" onClick={onClear} className={sc("input-clearable-icon")}/> : null}
+            {clearIconVisible
+                ? <Icon name="clear"
+                        onClick={onClear}
+                        className={classes(sc("input-icon"), props.businessType === 'password' ? sc('password-clear-icon') : '')}/>
+                : null}
+            {props.businessType === 'password' ?
+                <Icon name={`${passwordType === 'show' ? 'eye_open' : 'eye_close'}`}
+                      onClick={onPasswordEyeToggle}
+                      className={sc("input-icon")}/>
+                : null}
         </div>
     );
 };
